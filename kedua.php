551b7bcdf0d168578b28a9ee1b439ad80a1c263b
RayaBilfikri <rayabilfikri005@gmail.com>
2024-09-03 19:36:40 +0700

    Praktikum 2C - Function Sederhana

--- kedua.php
+++ kedua.php
@@ -1,60 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2 align="center">Selamat Datang</h2>
-    <p align="center">Sistem Pembayaran Gaji Karyawan</p>
-    <p>Silahkan masukkan jam kerja anda</p>
-    <form action="kedua.php" method="post">
-        <label>Jam kerja: </label>
-        <input type="number" name="jam_kerja"><br>
-        <label>apakah anda lembur? (ya/tidak)</label>
-        <input type="text" name="lemburga">
-        <input type="submit" value="calculate">
-    </form>
-</body>
-</html>
 <?php
-    $jam_kerja = $_POST["jam_kerja"];
-    $lembur = $_POST["lemburga"];
-    $bayaran = 65000;
-    $kalo_lembur = 25000;
-    
-    $total = $_POST["jam_kerja"]*$bayaran;
-    if ($_POST["lemburga"] == "ya") {
-        $hitungan_lembur = $total+$kalo_lembur;
-        echo "Bayaran anda sebesar Rp $hitungan_lembur";
-    }
-    elseif ($_POST["lemburga"] == "tidak") {
-        echo "Bayaran anda sebesar Rp $total";
-    }
-?>
 
+// Fungsi untuk menghitung keliling lingkaran
+function kelilingLingkaran($jari) : float {
+    $keliling = 2 * 3.14 * $jari;
+    return $keliling;
+}
 
-<?php 
-    class Produk {
-        //property
-        public $merk,
-        $tipe,
-        $tahun_produksi;
+// Fungsi untuk menghitung volume bola
+function volumeBola($jari) : float {
+    $volume = (4 / 3) * 3.14 * pow($jari, 3);
+    return $volume;
+}
 
-        public function __construct($merk,$tipe,$tahun_produksi)
-        {
-        $this->merk = $merk;
-        $this->tipe = $tipe;
-        $this->tahun_produksi = $tahun_produksi;
-        }
+// Fungsi untuk menghitung volume tabung
+function volumeTabung($jari, $tinggi) : float {
+    $volume = 3.14 * pow($jari, 2) * $tinggi;
+    return $volume;
+}
 
-        public function getlabel()
-        {
-            return "$this->merk,$this->tipe,$this->tahun_produksi"; //tes nanti
-        }
-    }
+// Fungsi untuk menghitung volume kerucut
+function volumeKerucut($jari, $tinggi) : float {
+    $volume = (1 / 3) * 3.14 * pow($jari, 2) * $tinggi;
+    return $volume;
+}
 
-    $produk1 = new Produk ("bmw","m5 comp",1995);
-    echo $produk1->getlabel();
-?>
+$jari = 45;
+$tinggi_tabung = 100;
+$tinggi_kerucut = 50;
+
+$keliling = kelilingLingkaran($jari);
+$volume_bola = volumeBola($jari);
+$volume_tabung = volumeTabung($jari, $tinggi_tabung);
+$volume_kerucut = volumeKerucut($jari, $tinggi_kerucut);
+
+echo "Keliling lingkaran dengan jari-jari {$jari} adalah {$keliling}\n";
+echo "Volume bola dengan jari-jari {$jari} adalah {$volume_bola}\n";
+echo "Volume tabung dengan jari-jari {$jari} dan tinggi {$tinggi_tabung} adalah {$volume_tabung}\n";
+echo "Volume kerucut dengan jari-jari {$jari} dan tinggi {$tinggi_kerucut} adalah {$volume_kerucut}\n";
