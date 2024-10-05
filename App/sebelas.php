545438fb795ad553ef857c32f4a8f11b2082840e
RayaBilfikri <rayabilfikri005@gmail.com>
2024-10-05 22:51:16 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

---
+++ App/sebelas.php
@@ -0,0 +1,7 @@
+<?php
+use App\Model\Akademik\Dosen;
+require_once "vendor/autoland.php";
+$dian = new Dosen (198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405",);
+$dian->mengajar();
+echo $dian->getNoHp();
+?>
